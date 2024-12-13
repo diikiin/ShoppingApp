54f91143f14244a0d31fcfdbd94738acd524c45c
Dauren <qabyl.da2@altynbank.kz>
2024-12-13 12:16:28 +0500

    user profile

--- app/src/main/java/com/dikin/shoppingapp/screens/UserProfileScreen.kt
+++ app/src/main/java/com/dikin/shoppingapp/screens/UserProfileScreen.kt
@@ -1,28 +1,38 @@
 package com.dikin.shoppingapp.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.dikin.shoppingapp.viewmodels.UserViewModel
+import com.dikin.shoppingapp.entities.User
 
 @Composable
-fun UserProfileScreen(userViewModel: UserViewModel) {
-    val user = userViewModel.currentUser!!
+fun UserProfileScreen() {
+//    val user = userViewModel.currentUser!!
+    val user = User(0, "username", "email", "passwordHash")
+
+    Spacer(Modifier.height(12.dp))
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Username: ${user.username}", style = MaterialTheme.typography.displayLarge)
+        Text(text = "Username: ${user.username}", style = MaterialTheme.typography.titleLarge)
         Text(text = "Email: ${user.email}", style = MaterialTheme.typography.bodyMedium)
     }
+}
+
+@Preview
+@Composable
+fun UserProfilePreview() {
+    UserProfileScreen()
 }
